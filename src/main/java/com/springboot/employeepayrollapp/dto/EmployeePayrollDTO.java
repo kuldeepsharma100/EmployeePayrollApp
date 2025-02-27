b1eba54bb7dae1be2a1cbd0708923d2e837e5537
kuldeepsharma100 <149848065+kuldeepsharma100@users.noreply.github.com>
2025-02-27 16:48:54 +0530

    [Kuldeep] add. Validate employee data with annotations

--- src/main/java/com/springboot/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/springboot/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -3,8 +3,14 @@ package com.springboot.employeepayrollapp.dto;
 
 import lombok.Data;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 @Data
 public class EmployeePayrollDTO {
+
+    @NotEmpty(message = "Name cannot be empty")
+    @Pattern(regexp = "^[A-Z][a-zA-Z\\s]{2,}$", message = "Invalid name format. Name must start with an uppercase letter and have at least 3 characters.")
     private String name;
+
     private double salary;
 }
