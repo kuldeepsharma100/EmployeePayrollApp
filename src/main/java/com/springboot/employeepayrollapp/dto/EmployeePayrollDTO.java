7e1c8475a72d8d00f1e14d3305c71b12ce3b9da9
kuldeepsharma100 <149848065+kuldeepsharma100@users.noreply.github.com>
2025-02-27 13:31:42 +0530

    [kuldeep sharma] add . Introduce Employee DTO and Model

---
+++ src/main/java/com/springboot/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -0,0 +1,11 @@
+package com.springboot.employeepayrollapp.dto;
+
+public class EmployeePayrollDTO {
+    public String name;
+    public double salary;
+
+    public EmployeePayrollDTO(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+}
