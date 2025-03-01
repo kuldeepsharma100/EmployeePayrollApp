865e5dc3595c0eb18b780f109ff2569f29d3c373
kuldeepsharma100 <149848065+kuldeepsharma100@users.noreply.github.com>
2025-03-01 10:28:43 +0530

    [kuldeep] add. Implemented full CRUD operations: Create, Read, Update, and Delete for EmployeePayroll

--- src/main/java/com/springboot/employeepayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/springboot/employeepayrollapp/controller/EmployeePayrollController.java
@@ -6,7 +6,7 @@ import com.springboot.employeepayrollapp.service.EmployeePayrollService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import lombok.extern.slf4j.Slf4j;
-
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -15,6 +15,18 @@ public class EmployeePayrollController {
 
     @Autowired
     private EmployeePayrollService employeePayrollService;
+
+    @GetMapping("/getAll")
+    public List<EmployeePayroll> getAllEmployees() {
+        log.info("Received request to fetch all employees.");
+        return employeePayrollService.getAllEmployees();
+    }
+
+    @GetMapping("/get/{id}")
+    public EmployeePayroll getEmployeeById(@PathVariable int id) {
+        log.info("Received request to fetch employee with ID: {}", id);
+        return employeePayrollService.getEmployeeById(id);
+    }
 
     @PostMapping("/create")
     public EmployeePayroll createEmployee(@Valid @RequestBody EmployeePayrollDTO dto) {
@@ -26,5 +38,11 @@ public class EmployeePayrollController {
     public EmployeePayroll updateEmployee(@PathVariable int id, @Valid @RequestBody EmployeePayrollDTO dto) {
         log.info("Received request to update employee ID: {} with data: {}", id, dto);
         return employeePayrollService.updateEmployee(id, dto);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        log.info("Received request to delete employee with ID: {}", id);
+        return employeePayrollService.deleteEmployee(id);
     }
 }
